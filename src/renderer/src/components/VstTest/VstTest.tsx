55285c55cdfda8f3b9fe4e5d69df385d8c97beae
ecof <ecof1911@gmail.com>
2023-06-06 20:33:32 +0900

    feat: 볼륨 조절 및 음소거 추가

--- src/renderer/src/components/VstTest/VstTest.tsx
+++ src/renderer/src/components/VstTest/VstTest.tsx
@@ -6,6 +6,8 @@ export const VstTest: React.FC = () => {
   const gainNodeRef = useRef<GainNode | null>(null);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
   const [latency, setLatency] = useState<string>('');
+  const [volume, setVolume] = useState<number>(100);
+  const [mute, setMute] = useState<boolean>(false);
 
   useEffect(() => {
     const startLatencyMeasurement = async () => {
@@ -34,8 +36,7 @@ export const VstTest: React.FC = () => {
           const sampleRate = audioContextRef.current.sampleRate;
           const latency = Math.round((bufferLength / sampleRate) * 1000);
           setLatency(`레이턴시: ${latency}ms, 최대 진폭: ${maxAmplitude}`);
-          console.log(`레이턴시: ${latency}ms, 최대 진폭: ${maxAmplitude}`);
-        }, 1000);
+        }, 100);
       } catch (error) {
         console.error('마이크 스트림을 얻는 동안 오류가 발생했습니다:', error);
       }
@@ -56,10 +57,33 @@ export const VstTest: React.FC = () => {
     return stopLatencyMeasurement;
   }, []);
 
+  useEffect(() => {
+    gainNodeRef.current?.gain.setValueAtTime(
+      volume / 100,
+      audioContextRef.current?.currentTime || 0
+    );
+  }, [volume]);
+
+  useEffect(() => {
+    if (!audioContextRef.current?.destination) return;
+    if (mute) gainNodeRef.current?.disconnect();
+    else gainNodeRef.current?.connect(audioContextRef.current?.destination);
+  }, [mute]);
+
   return (
     <>
       <p>VST TEST</p>
       <p>{latency}</p>
+      <input
+        type="range"
+        max="100"
+        min="0"
+        value={volume}
+        onChange={(e) => setVolume(+e.target.value)}
+      />
+      <button onClick={() => setMute(!mute)} className="block border p-2">
+        {mute ? 'mute' : 'no mute'}
+      </button>
     </>
   );
 };
